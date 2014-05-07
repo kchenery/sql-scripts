a38e8316a45016937bca714fc79cd490b2a0fc91
Kent Chenery <kent.chenery@xero.com>
2014-05-07 15:25:01 +1200

    sp_helpindex2, sp_helpfile2, sp_useroptions

---
+++ sp_helpfile2.sql
@@ -0,0 +1,50 @@
+﻿USE [master];
+GO
+
+IF NOT EXISTS(SELECT 1 FROM sys.procedures WHERE object_id = OBJECT_ID('dbo.sp_helpfile2'))
+BEGIN;
+	EXEC('CREATE PROCEDURE sp_helpfile2 AS SELECT ''Procedure Stub''');
+END;
+GO
+
+ALTER PROCEDURE sp_helpfile2
+AS
+/*
+---
+Description:    Returns information about database files in the current database
+
+Parameters:		none
+
+Output:
+ - Similar information to sp_helpfile.  Extra columns such as amount of space in the file that is used.
+...
+*/
+BEGIN
+	/* Turn off row counting */
+	SET NOCOUNT ON;
+
+	/* Reture the data */
+	SELECT
+		file_id
+		,type_desc															AS FileType
+		,state_desc															AS FileState
+		,name																AS LogicalName
+		,CAST(size / 128.0 AS DECIMAL(10, 2))								AS SizeMB
+		,CASE is_percent_growth
+			WHEN 0 THEN CAST(CAST(growth / 128.0 AS DECIMAL(10, 2)) AS NVARCHAR(20)) + ' MB'
+			WHEN 1 THEN CAST(growth AS NVARCHAR(20)) + ' %'
+		END																	AS Growth
+		,CAST(IIF(max_size < 0, NULL, max_size / 128.0) AS DECIMAL(10, 2))	AS MaxSizeMB
+		,CAST(FILEPROPERTY(name, 'SpaceUsed') / 128.0 AS DECIMAL(10, 2))	AS UsedMB
+		,CAST((CAST(FILEPROPERTY(name, 'SpaceUsed') / 128.0 AS DECIMAL(10, 2))) * 100 / CAST(size / 128.0 AS DECIMAL(10, 2)) AS DECIMAL(10, 2))	AS UsedPct
+		,physical_name
+		,is_read_only
+		,is_media_read_only
+		,is_sparse
+	FROM
+		sys.database_files
+END;
+GO
+
+EXEC sys.sp_MS_marksystemobject 'sp_helpfile2'
+GO
---
+++ sp_useroptions.sql
@@ -0,0 +1,44 @@
+USE [master]
+GO
+
+IF NOT EXISTS(SELECT 1 FROM sys.procedures WHERE object_id = OBJECT_ID('dbo.sp_useroptions'))
+BEGIN;
+    EXEC('CREATE PROCEDURE sp_useroptions AS SELECT ''ProcedureStub'' AS Stub;');
+END;
+GO
+
+ALTER PROCEDURE sp_useroptions
+AS
+/*
+---
+Description:    Decodes the @@options values according to http://technet.microsoft.com/en-us/library/ms177525.aspx
+
+Parameters:
+ - None
+
+Output:
+ - Single row that decodes the @@option values into flags for each option that can be set
+...
+*/
+BEGIN;
+	SELECT
+		(@@OPTIONS & 1) / 1				AS DISABLE_DEF_CNST_CHK
+		,(@@OPTIONS & 2) / 2			AS IMPLICIT_TRANSACTIONS
+		,(@@OPTIONS & 4) / 4			AS CURSOR_CLOSE_ON_COMMIT
+		,(@@OPTIONS & 8) / 8			AS ANSI_WARNINGS
+		,(@@OPTIONS & 16) / 16			AS ANSI_PADDING
+		,(@@OPTIONS & 32) / 32			AS ANSI_NULLS
+		,(@@OPTIONS & 64) / 64			AS ARITHABORT
+		,(@@OPTIONS & 128) / 128		AS ARITHIGNORE
+		,(@@OPTIONS & 256) / 256		AS QUOTED_IDENTIFIER
+		,(@@OPTIONS & 512) / 512		AS NOCOUNT
+		,(@@OPTIONS & 1024) / 1024		AS ANSI_NULL_DFLT_ON
+		,(@@OPTIONS & 2048) / 2048		AS ANSI_NULL_DFLT_OFF
+		,(@@OPTIONS & 4096) / 4096		AS CONCAT_NULL_YIELDS_NULL
+		,(@@OPTIONS & 8192) / 8192		AS NUMERIC_ROUNDABORT
+		,(@@OPTIONS & 16384) / 16384	AS XACT_ABORT
+END;
+GO
+
+EXEC sys.sp_MS_marksystemobject 'sp_useroptions'
+GO
